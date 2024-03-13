2afa562aedd87825e671b259d62c991e8a908c1e
Pranav Dumbre <pranavdumbre1911@gmail.com>
2024-03-13 17:38:48 +0530

    D

--- Flutter Projects/todowithsqlflite/lib/main.dart
+++ Flutter Projects/todowithsqlflite/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'advToDoUI.dart';
+import 'package:todowithsqlflite/login.dart';
 
 void main() async {
   runApp(const MainApp());
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Advtodo(),
+      home: Login(),
     );
   }
 }
